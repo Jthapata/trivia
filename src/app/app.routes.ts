c0f940a23faee45d3d96d38c24f5b1ea75c1c4ea
MattBailey <mattbaileywork@gmail.com>
2024-10-09 21:15:37 -0600

    got number of players, added place holders for existing users, and submit button

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { GameSetupComponent } from './game-setup/game-setup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'game-setup', component: GameSetupComponent },
+];
