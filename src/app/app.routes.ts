19097d298bc199b622a11dec0188a41d3a7bd591
Matthew Bailey <matthewbailey.webdev@gmail.com>
2024-10-10 10:04:47 -0600

    premerge commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { GameSetupComponent } from './game-setup/game-setup.component';
+import { GameSetupComponent } from './components/game-setup/game-setup.component';
 
 export const routes: Routes = [
   { path: 'game-setup', component: GameSetupComponent },
