8660074026b9b1d2c60438438c4321f796a5646b
jthapata <jessezap01@gmail.com>
2024-10-14 20:59:57 -0600

    getting questions from api and set up questions service

---
+++ src/app/services/questions.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import {Question} from "../interfaces/question";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuestionsService {
+  private questions: Question[] = [];
+  private currentQuestion: Question | undefined;
+  private currentAnswers: string[] = [];
+
+  constructor() { }
+
+  setQuestions(questions: Question[]) {
+    this.questions = questions;
+    if (this.questions.length > 0) {
+      this.currentQuestion = this.questions.shift();
+    }
+    this.mixAnswers();
+  }
+
+  getCurrentQuestion() : Question | undefined {
+    return this.currentQuestion;
+  }
+
+  getCurrentAnswers() : string[] {
+    return this.currentAnswers;
+  }
+
+  nextQuestion() {
+    if (this.questions.length > 0) {
+      this.currentQuestion = this.questions.shift();
+    }
+  }
+
+  mixAnswers() {
+    if (this.currentQuestion) {
+      let answers = [...this.currentQuestion.incorrect_answers];
+      answers.push(this.currentQuestion.correct_answer);
+      answers = this.shuffle(answers);
+      this.currentAnswers = answers;
+    }
+  }
+
+  private shuffle(answers: string[]) {
+    for (let i = answers.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [answers[i], answers[j]] = [answers[j], answers[i]];
+    }
+    return answers;
+  }
+}
